7b8225c287422b249eb696def0a7caa555fc9715
Chocks <chocks.eswaramurthy@muckrack.com>
2023-12-01 11:25:09 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+
+
+def get_numbers():
+    return random.sample(range(1, 3), k=2)
---
+++ test_main.py
@@ -0,0 +1,24 @@
+import unittest
+from typing import List
+
+from parameterized import parameterized
+
+from main import get_numbers
+
+
+class RandomNumberTestCase(unittest.TestCase):
+    @parameterized.expand([
+        ([1, 2], '')
+    ])
+    def test_get_numbers_flaky(self, expected_list: List[int], _):
+        self.assertListEqual(get_numbers(), expected_list)
+
+    @parameterized.expand([
+        ([1, 2], '')
+    ])
+    def test_get_numbers_good(self, expected_list: List[int], _):
+        self.assertListEqual(sorted(get_numbers()), sorted(expected_list))
+
+
+if __name__ == '__main__':
+    unittest.main()
